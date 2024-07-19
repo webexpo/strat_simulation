a050a9e18de5cfd1867d1edd021df5cd9924aacf
Jérôme Lavoué <jerome.lavoue@umontreal.ca>
2024-07-19 15:22:47 -0400

    [EXIL TD] reorg of scripts, all detected test run done. all detected run1 started

---
+++ full simulations/TD EXIL 2024 measurement error impact/scripts/one_scenario_functions.R
@@ -0,0 +1,129 @@
+# Functions for performing a simulation study with one scenario
+
+
+
+##### FUNCTIONS ####
+
+#' function which analyses the ith pair of samples ( clean and dirtied ) in the simulation 
+#'
+#' @param index index of the simulation 
+#' @param simulated_data_object list containing the simulated data for a single scenario 
+#' @param me.cv value for the measurement error as standard error CV, not in % and not expanded uncertainty CV
+#' @param oel occupational exposure limit
+#' @param n_iterations number of iteration for the GUM approach
+#' @param sim_quantile quantile selection for the summaries of metrics across iterations in addition to mean and median
+#'
+#' @return matrix of results with one column per approach
+#'
+
+
+ithpair.function <- function( index , simulated_data_object , me_cv , n_iterations_gum = 10000 , sim_quantile = 0.95, oel ) {
+  
+  # data preparation
+  
+  true_data <- simulated_data_object$true[,index]
+  
+  observed_data <- simulated_data_object$observed[,index]
+  
+  ## analysis
+  
+  ideal_b = expostats.naive( true_data , oel)
+  ideal_f = frequentist.naive( true_data , oel)
+  naive_b = expostats.naive( observed_data , oel)
+  naive_f = frequentist.naive( observed_data , oel)
+  me_b = expostats.me( observed_data , oel , me_cv)
+  me_f = frequentist.me( observed_data , oel , me_cv , n_iterations_gum , sim_quantile ) # is a list of 3 vectors (mean, median,quantile)
+  
+  ## results as a single matrix
+  
+  one_column <- c("gm_est","gsd_est","p95_est","p95_70ucl","p95_95ucl","F_est","F_70ucl","F_95ucl")
+  
+  results <- matrix( c( ideal_b , ideal_f , naive_b , naive_f , me_b , me_f$mean , me_f$median , me_f$quantile ) , nrow = length(one_column) )
+
+  colnames(results) <- c("ideal_b","ideal_f","naive_b","naive_f","me_b","me_f_mean","me_f_median","me_f_quantile")
+  rownames(results) <- one_column
+  
+  return(results)
+  
+}
+
+
+#' function which uses parallel computing to perform the simulation for one data generation object (one scenario)
+#'
+#' @param index index of the simulation 
+#' @param simulated_data_object list containing the simulated data for a single scenario 
+#' @param me.cv value for the measurement error as standard error CV, not in % and not expanded uncertainty CV
+#' @param oel occupational exposure limit
+#' @param n_iterations number of iteration for the GUM approach
+#' @param sim_quantile quantile selection for the summaries of metrics across iterations in addition to mean and median
+#'
+#' @return matrix of results with one column per approach
+#'
+
+
+parallel.function <- function( simulated_data_object , me_cv , n_iterations_gum = 10000 , sim_quantile = 0.95 , n_sim , n_clusters = 10, oel ) {
+  
+  # compteur de temps initialisé
+  start_time <- Sys.time()
+  
+  
+  #procédure parallele
+  
+  # creating the clusters ( use detectCores() to count the cores available, choose this number minus 2 or 4 below) 
+  
+  cl <- makeCluster(n_clusters)
+  
+  # libraries and scripts to be used in each cluster
+  
+  clusterEvalQ(cl, library(rjags))
+  clusterEvalQ(cl, library(tolerance))
+  
+  # sending objects to clusters
+  clusterExport( cl , "jags.model.informedvar" , envir=environment())
+  clusterExport( cl , "webexpo.seg.datapreparation" , envir=environment())
+  clusterExport( cl , "Webexpo.seg.globalbayesian.jags" , envir=environment())
+  clusterExport( cl , "fun.jags.informedvar" , envir=environment())
+  clusterExport( cl , "expostats.naive" , envir=environment())
+  clusterExport( cl , "expostats.me" , envir=environment())
+  clusterExport( cl , "frequentist.naive" , envir=environment())
+  clusterExport( cl , "frequentist.me" , envir=environment())
+  clusterExport( cl , "ithpair.function" , envir=environment())
+  clusterExport( cl , "fun.NdExpo.lognorm" , envir=environment())
+  clusterExport( cl , "fun.perc.en689" , envir=environment())
+  clusterExport( cl , "fun.frac.dep" , envir=environment())
+  
+  
+  clusterExport( cl , "oel" , envir=environment())
+  clusterExport( cl , "me_cv" , envir=environment())
+  clusterExport( cl , "n_iterations_gum" , envir=environment())
+  clusterExport( cl , "sim_quantile" , envir=environment())
+  clusterExport( cl , "simulated_data_object" , envir=environment())
+  # calculations
+  
+  simulation_result_parallel <- parLapply(cl, X = as.list(1:n_sim) , function(x){ ithpair.function(x, 
+                                                                                                   simulated_data_object = simulated_data_object , 
+                                                                                                   me_cv = me_cv , 
+                                                                                                   n_iterations_gum = n_iterations_gum , 
+                                                                                                   sim_quantile = sim_quantile,
+                                                                                                   oel = oel) } ) 
+  
+  # recommendation from the net: close the clusters
+  stopCluster(cl)
+  
+  # making an array of the results
+  
+  simulation_result_parallel_array <- array( data = unlist(simulation_result_parallel) , dim = c( 8 , 8 , n_sim ) )
+  
+  # estimation of computing time ( 9 min on my computer for 5000 iterations)
+  end_time <- Sys.time()
+  mytime <- end_time - start_time 
+  
+  # results
+  
+  results <- list( array = simulation_result_parallel_array , time = mytime )
+  
+  return(results)
+  
+}
+  
+
---
+++ full simulations/TD EXIL 2024 measurement error impact/scripts/stability_test.R
@@ -0,0 +1,62 @@
+# Script for assessing the stability of the simulation 
+
+
+##### LIBRARIES ####
+
+
+##### DATA ####
+
+
+#test generated in script one_scenario_test.R
+
+test_repeatability <- readRDS("full simulations/TD EXIL 2024 measurement error impact/data/test_repeatability1.RDS")
+
+##### SCRIPTS ####
+
+source("full simulations/TD EXIL 2024 measurement error impact/scripts/simulation_interpretation.R")
+source("other/performance_metrics.R")
+
+##### TEST 1 ####
+
+### parameters
+
+true_gsd <- 2.5
+
+true_exceedance_perc <- 2.5  ## in percentage
+
+true_p95 <- 100
+
+true_gm <- exp( log(true_p95) - qnorm(0.95)*log(true_gsd) )
+
+oel <- exp( qnorm(1 - true_exceedance_perc/100, mean = log(true_gm) , sd = log(true_gsd) ) )
+
+sample_size <- 6
+
+n_sim <- 5000
+
+me_cv <- 0.25
+
+n_iterations_gum = 5000
+
+sim_quantile = 0.95
+
+### analysis
+
+test_rmse <- rmse.result( results_one_scenario = test_repeatability[[1]] , 
+                          true_gm = true_gm , true_gsd = true_gsd , true_p95 = true_p95 , 
+                          true_exceedance_perc = true_exceedance_perc )
+
+
+rmse_array <- array( dim=c(8,4,5) )
+
+for (i in 1:5) { rmse_array[,,i] <- as.matrix(rmse.result( results_one_scenario = test_repeatability[[i]] , 
+                                                           true_gm = true_gm , true_gsd = true_gsd , true_p95 = true_p95 , 
+                                                           true_exceedance_perc = true_exceedance_perc )[,2:5]) }
+
+result_meansd <-apply( rmse_array , c(1,2) , function(x) { paste( signif(mean(x),2) , 
+                                                  "(", signif(100*sd(x)/mean(x),2),")", sep="")})
+                                                  
+                                                  
+result_minmax <-apply( rmse_array , c(1,2) , function(x) { paste("[", signif(min(x),2) , "-" , signif(max(x),2) , "]",
+                                                  sep="") } )
+
